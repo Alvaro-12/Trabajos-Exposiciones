2bd0fd893a47c5fadcbf767b1203b84143fda4cf
Alvaro Lopez <lt0895032020@unab.edu.sv>
2021-05-09 19:02:09 -0600

    Trabajo Grupo 2

---
+++ Grupo2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Ingrese su nombre:");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su sexo: (Hombre = H.   Mujer = M)");
+            char sexo = char.Parse(Console.ReadLine());
+
+
+            if (sexo == char.Parse("H"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("El Sr. {0} se ha registrado", nombre);
+            }
+            if (sexo == char.Parse("M"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("La Sra. {0} se ha registrado.", nombre);
+
+            }
+        
+        }
+    }
+}
